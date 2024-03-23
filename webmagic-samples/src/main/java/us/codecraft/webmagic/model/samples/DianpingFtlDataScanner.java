13a4cd66c83e1dc5c1419b163aa4de2ad0656a68
mimib <camille.barthelemy59@gmail.com>
2024-03-23 19:10:58 +0100

    remplacement des nombres magiques par des constantes de classes

--- webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/DianpingFtlDataScanner.java
+++ webmagic-samples/src/main/java/us/codecraft/webmagic/model/samples/DianpingFtlDataScanner.java
@@ -17,20 +17,24 @@ import java.util.List;
 @TargetUrl("http://*.alpha.dp/*")
 public class DianpingFtlDataScanner implements AfterExtractor {
 
+	private static final int MIN_DATA_SIZE_TO_PRINT = 1;
+	private static final int MIN_FIRST_DATA_LENGTH_TO_PRINT = 100;
+	private static final int THREAD_COUNT = 5;
+
 	@ExtractBy(value = "(DP\\.data\\(\\{.*\\}\\));", type = ExtractBy.Type.Regex, notNull = true, multi = true)
 	private List<String> data;
 
 	public static void main(String[] args) {
 		OOSpider.create(Site.me().setSleepTime(0), DianpingFtlDataScanner.class)
-				.thread(5).run();
+				.thread(THREAD_COUNT).run();
 	}
 
 	@Override
 	public void afterProcess(Page page) {
-		if (data.size() > 1) {
+		if (data.size() > MIN_DATA_SIZE_TO_PRINT) {
 			System.err.println(page.getUrl());
 		}
-		if (data.size() > 0 && data.get(0).length() > 100) {
+		if (data.size() > 0 && data.get(0).length() > MIN_FIRST_DATA_LENGTH_TO_PRINT) {
 			System.err.println(page.getUrl());
 		}
 	}
